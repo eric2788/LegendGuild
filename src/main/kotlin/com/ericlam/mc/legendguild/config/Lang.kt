34150aee16dfcc8600e8af84823d51cd07a60f1f
eric2788 <tsukiko852@gmail.com>
2020-01-19 00:22:09 +0800

    add prefix

--- src/main/kotlin/com/ericlam/mc/legendguild/config/Lang.kt
+++ src/main/kotlin/com/ericlam/mc/legendguild/config/Lang.kt
@@ -1,7 +1,9 @@
 package com.ericlam.mc.legendguild.config
 
+import com.ericlam.mc.kotlib.config.Prefix
 import com.ericlam.mc.kotlib.config.Resource
 import com.ericlam.mc.kotlib.config.dto.MessageFile
 
 @Resource(locate = "lang.yml")
+@Prefix(path = "prefix")
 class Lang : MessageFile()
